ae77b2f1a30200c396dd5a8b04ff95cd8596c1c1
PoVeDa <poveda_gandia@yahoo.es>
2011-01-06 19:51:29 +0100

    Implement Anticheat AntiLag System Kick

--- src/server/game/World/World.h
+++ src/server/game/World/World.h
@@ -755,6 +755,12 @@ class World
         static int32 GetVisibilityNotifyPeriodOnContinents(){ return m_visibility_notify_periodOnContinents; }
         static int32 GetVisibilityNotifyPeriodInInstances() { return m_visibility_notify_periodInInstances;  }
         static int32 GetVisibilityNotifyPeriodInBGArenas()  { return m_visibility_notify_periodInBGArenas;   }
+        // movement anticheat
+        static bool GetEnableMvAnticheat()       { return m_EnableMvAnticheat;     }
+        static uint32 GetTeleportToPlaneAlarms() { return m_TeleportToPlaneAlarms; }
+        static uint32 GetMistimingDelta()        { return m_MistimingDelta;        }
+        static uint32 GetMistimingAlarms()       { return m_MistimingAlarms;       }
+        // end movement anticheat
 
         void SetWintergrapsTimer(uint32 timer, uint32 state)
         {
@@ -881,6 +887,12 @@ class World
         static int32 m_visibility_notify_periodOnContinents;
         static int32 m_visibility_notify_periodInInstances;
         static int32 m_visibility_notify_periodInBGArenas;
+
+	// movement anticheat enable flag
+        static bool m_EnableMvAnticheat;
+        static uint32 m_TeleportToPlaneAlarms;
+        static uint32 m_MistimingDelta;
+        static uint32 m_MistimingAlarms;	
 
         //movement anticheat enable flag
         bool m_MvAnticheatEnable;
