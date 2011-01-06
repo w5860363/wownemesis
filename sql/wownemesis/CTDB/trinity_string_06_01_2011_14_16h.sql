7fe14e10edb2e8ee2e97b079a24fdabae9cad6dd
Gandia <poveda_gandia@yahoo.es>
2011-01-06 14:21:12 +0100

    Fix trinity_string kick system

--- sql/wownemesis/CTDB/trinity_string_06_01_2011_14_16h.sql
+++ sql/wownemesis/CTDB/trinity_string_06_01_2011_14_16h.sql
@@ -1,2 +1,2 @@
-DELETE FROM `trinity_string` WHERE `entry`='1140';
-INSERT INTO `trinity_string` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`) values('1140','|cffffffff%s |cffffffffwas kicked by %s. (%s)',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+DELETE FROM `trinity_string` WHERE `entry`='1139';
+INSERT INTO `trinity_string` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`) values('1139','|cffffffff%s |cffffffffwas kicked by %s. (%s)',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
